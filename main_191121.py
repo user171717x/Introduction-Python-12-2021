d315eb656ea52687e082ed78de66cd28fec78dfa
user171717x <user171717user@protonmail.com>
2021-12-08 20:39:14 +0200

    homework 161121 done

---
+++ main_191121.py
@@ -0,0 +1,51 @@
+print("""
+Собственно имя: SERHIO
+
+Эскиз:
+            
+          * * *                           * * *                           * * *                 *                      *        *       *                 * * *
+       *          *                    *          *                    *          *             *                      *          *   *                *          *
+    *                *              *                *              *                *          *                      *            *               *                *
+  *                    *          *                    *          *                    *        *                      *            *             *                    *
+ *                               *                               *                      *       *                      *            *            *                      *
+ *                               *                               *                      *       *                      *            *            *                      *
+  *                               *                              **                    *        *                      *            *            *                      *
+    *                               *                            *  *                *          *                      *            *            *                      *
+        *                               *                        *  *  *          *             *  *                *  *            *            *                      *
+            *                               *                    *    *    * * *                *     ** * ** * **     *            *            *         ****         *
+                *                       *                        *       *                      *  *                *  *            *            *                      *
+                   *                *                            *          *                   *                      *            *            *                      *
+                     *            *                              *             *                *                      *            *            *                      *
+                      *          *                               *               *              *                      *            *            *                      *
+                      *          *                               *                 *            *                      *            *            *                      *
+ *                   *            *                    *         *                   *          *                      *            *             *                    *
+   *               *                *                *           *                    *         *                      *            *               *                *
+      *         *                      *          *              *                     *        *                      *          *   *                *          *
+         * * *                            * * *                  *                     *        *                      *        *       *                 * * *
+
+
+Реализация:
+""")
+
+
+print("\
+\t\t  * * * \t\t\t\t\t\t  * * * \t\t\t\t\t\t  * * * \t\t\t\t*   \t\t\t\t   *\t\t*   \t*   \t\t\t  * * *\n\
+\t   *\t\t  * \t\t\t\t   *\t\t  * \t\t\t\t   *\t\t  * \t\t\t*   \t\t\t\t   *\t\t  *   * \t\t\t   *\t\t  *\n\
+\t*   \t\t\t *  \t\t\t*   \t\t\t *  \t\t\t*   \t\t\t *  \t\t*   \t\t\t\t   *\t\t\t*   \t\t\t*   \t\t\t *\n\
+  * \t\t\t\t   *\t\t  * \t\t\t\t   *\t\t  * \t\t\t\t   *\t\t*   \t\t\t\t   *\t\t\t*   \t\t  * \t\t\t\t   *\n\
+ *  \t\t\t\t\t\t\t *  \t\t\t\t\t\t\t *  \t\t\t\t\t*   \t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+ *  \t\t\t\t\t\t\t *  \t\t\t\t\t\t\t *  \t\t\t\t\t*   \t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+  * \t\t\t\t\t\t\t  * \t\t\t\t\t\t\t ** \t\t\t\t   *\t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t*   \t\t\t\t\t\t\t*   \t\t\t\t\t\t *  *   \t\t\t *  \t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t*   \t\t\t\t\t\t\t*   \t\t\t\t\t *  *  *\t\t  * \t\t\t*  *\t\t\t\t*  *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t\t*   \t\t\t\t\t\t\t*   \t\t\t\t *    *    * * *\t\t\t\t*     ** * ** * **     *\t\t\t*   \t\t *  \t   **** \t\t*\n\
+\t\t\t\t*   \t\t\t\t\t*   \t\t\t\t\t *  \t *  \t\t\t\t\t*  *\t\t\t\t*  *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t\t\t   *\t\t\t\t*   \t\t\t\t\t\t *  \t\t*   \t\t\t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t\t\t\t *  \t\t  * \t\t\t\t\t\t\t *  \t\t   *\t\t\t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t\t\t\t  * \t\t *  \t\t\t\t\t\t\t *  \t\t\t *  \t\t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+\t\t\t\t\t  * \t\t *  \t\t\t\t\t\t\t *  \t\t\t   *\t\t\t*   \t\t\t\t   *\t\t\t*   \t\t *  \t\t\t\t\t*\n\
+ *  \t\t\t\t *  \t\t  * \t\t\t\t   *\t\t *  \t\t\t\t *  \t\t*   \t\t\t\t   *\t\t\t*   \t\t  * \t\t\t\t   *\n\
+   *\t\t\t   *\t\t\t\t*   \t\t\t *  \t\t *  \t\t\t\t  * \t\t*   \t\t\t\t   *\t\t\t*   \t\t\t*   \t\t\t *\n\
+\t  * \t\t*   \t\t\t\t   *\t\t  * \t\t\t *  \t\t\t\t   *\t\t*   \t\t\t\t   *\t\t  *   * \t\t\t   *\t\t  *\n\
+\t\t * * *  \t\t\t\t\t\t  * * * \t\t\t\t *  \t\t\t\t   *\t\t*   \t\t\t\t   *\t\t*   \t*   \t\t\t  * * *\n\
+")
