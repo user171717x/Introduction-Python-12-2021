81c4e19e689c3568967dae0886c081fec4089b4a
user171717x <user171717user@protonmail.com>
2022-01-11 22:17:34 +0200

    Homework 281221 done (Optimized)

--- main_281221/main_281221.py
+++ main_281221/main_281221.py
@@ -2,39 +2,74 @@
 import calendar
 
 
+# Delim logiky programmi, sozdali otdelnyu fynkciu kotoraya ispolzyetsya kagdoi drygoi fynkciey
+def read_file(filename):
+    with open(filename, 'r') as txt_file:
+        data = txt_file.read()
+        data = data.split('\n') # delim txt file na stroki
+    return data
+
+
 def get_domains_list(file_name: str) -> list:
-    with open(file_name, 'r') as text_file:
-        text = text_file.read().strip('.')
-    return text.split('\n.')
+    lines = read_file(file_name)
+    domains = [line.replace('.', '') for line in lines]
+    return domains
+    # with open(file_name, 'r') as text_file:
+    #     text = text_file.read().strip('.')
+    # return text.split('\n.')
 
 
 def get_lastnames_list(file_name: str) -> list:
-    with open(file_name, 'r') as text_file:
-        text = text_file.readlines()
-    return [line.split('\t')[1] for line in text]
+    lines = read_file(file_name)
+    names = [line.split('\t')[1] for line in lines]
+    return names
+    # with open(file_name, 'r') as text_file:
+    #     text = text_file.readlines()
+    # return [line.split('\t')[1] for line in text]
 
 
 def convert_date(date_str: str) -> str:
-    months = calendar.month_name[:]
-    date_fields = date_str.split()
-    day_year = date_fields[2]
-    day_month = str(months.index(date_fields[1])).zfill(2)  # adding needed 0 at start
-    day_day = date_fields[0][:-2].zfill(2)
-    return f"{day_day}/{day_month}/{day_year}"
+    ## GOT FROM ## commented lines
+    ## month_dict = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05',
+    ##               'June': '06', 'July': '07', 'August': '08', 'September': '09', 'October': '10',
+    ##              'November': '11', 'December': '12'}
+    month_dict = {month: str(idx).zfill(2) for idx, month in enumerate(calendar.month_name) if month}
+    day, month, year = date_str.split()
+    day = day[:-2].zfill(2)
+    month = month_dict[month]
+    return f"{day}/{month}/{year}"
+    # months = calendar.month_name[:]
+    # date_fields = date_str.split()
+    # day_year = date_fields[2]
+    # day_month = str(months.index(date_fields[1])).zfill(2)  # adding needed 0 at start
+    # day_day = date_fields[0][:-2].zfill(2)
+    # return f"{day_day}/{day_month}/{day_year}"
 
 
-def scrap_date_lines(file_name: str) -> list:
-    with open(file_name, 'r') as text_file:
-        text = text_file.readlines()
-    date_lines = list()
-    for line in text:
+def scrap_dates(filename):
+    authors = []
+    lines = read_file(filename)
+    ##count = 1
+    ##month_dict = {}
+    for line in lines:
         if ' - ' in line:
-            date_lines.append(line.split(' - ')[0])
-    return date_lines
-
-
-def scrap_dates(file_name: str) -> list:
-    return [{'date_original': date, 'date_modified': convert_date(date)} for date in scrap_date_lines(file_name)]
+            date_author = line.split(' - ')[0]
+            authors.append({"data_original": date_author, "data_modified": convert_date(date_author)})
+        ##elif ' - ' not in line and line:    # 2nd yslovie - oznachaet NE PYSTAYA STROKA
+        ##    month_dict[line] = str(count).zfill(2)
+        ##   count += 1
+    ##print(month_dict)
+    return authors
+# def scrap_date_lines(file_name: str) -> list:
+#     with open(file_name, 'r') as text_file:
+#         text = text_file.readlines()
+#     date_lines = list()
+#     for line in text:
+#         if ' - ' in line:
+#             date_lines.append(line.split(' - ')[0])
+#     return date_lines
+# def scrap_dates(file_name: str) -> list:
+#     return [{'date_original': date, 'date_modified': convert_date(date)} for date in scrap_date_lines(file_name)]
 
 
 """
